c059b4c0922ea407fe55b5bae281daa058999e96
unknown <antonstanaszek@gmail.com>
2024-11-19 13:08:57 +0100

    bambi-fail-test

--- src/components/pages/Bambi.tsx
+++ src/components/pages/Bambi.tsx
@@ -1,34 +1,5 @@
 const Bambi = () => {
-  return (
-    <div
-      style={{
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)", // Center the container
-        width: "60vw", // Adjust size as needed
-        height: "60vh", // Adjust size as needed
-        overflow: "hidden", // Hide any overflow if necessary
-        backgroundColor: "rgb(102, 102, 106)", // Set the background color
-      }}
-    >
-      <video
-        src="/bambi.mp4"
-        autoPlay
-        loop
-        muted={false}
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)", // Center the video inside the container
-          width: "100%", // Make the video fill the width
-          height: "100%", // Make the video fill the height
-          objectFit: "contain", // Ensure the video maintains its aspect ratio
-        }}
-      />
-    </div>
-  );
+  return <div>Bambi</div>;
 };
 
 export default Bambi;
